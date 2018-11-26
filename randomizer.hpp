25105aa3e722a2e596a49e43eb1e6491ebb40e23
Amy Hong <amyhong0502@gmail.com>
2018-11-25 22:46:45 -0800

    docs: write comments

--- randomizer.hpp
+++ randomizer.hpp
@@ -8,16 +8,45 @@
 #include <random>
 #include "tour.hpp"
 
+/**
+ * Helper to generate random factors.
+ */
 class randomizer {
+
+private:
+    /**
+     * Returns a randomly generated city.
+     *
+     * @param name name of the city
+     * @return randomly generated city.
+     */
+    city* generate_city(std::string name);
 
 public:
 
+    /**
+     * Returns a random integer in the given range.
+     *
+     * @param min Minimum value
+     * @param max Maximum value
+     * @return Random int in the given range
+     */
     int generate_random_int(int min, int max);
 
+    /**
+     * Returns a random double in the given range.
+     *
+     * @param min Minimum value
+     * @param max Maximum value
+     * @return Random double in the given range
+     */
     double generate_random_double(double min, double max);
 
-    city* generate_city(std::string name);
-
+    /**
+     * Generates a tour and return the tour.
+     *
+     * @return randomly generated tour
+     */
     tour generate_tour();
 };
 
